7a2cdc06d5db6031729b95f35ab3f28a7ef7114d
Sam <samantha.gerrity@gmail.com>
2021-03-23 23:57:21 +0000

    Includes classes
    Co-authored-by: Jenny Han <han.jennyjh@outlook.com>
    co-authored-by: Alena Davis <alenad@gmail.com>

--- model.py
+++ model.py
@@ -18,6 +18,60 @@ def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
 
     print('Connected to the db!')
 
+class User(db.Model):
+    """A user table class"""
+
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    email = db.Column(db.String, 
+                        nullable=False, 
+                        unique=True)
+    password = db.Column(db.String, 
+                        nullable=False, 
+                        unique=True)
+            
+    def __repr__(self):
+        return f'<User user_id={self.user_id} email={self.email} Also, you look great today.>'
+
+class Movie(db.Model):
+    """Movie table class"""
+
+    __tablename__ = 'movies'
+    
+    movie_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    title = db.Column(db.String)
+    overview = db.Column(db.Text)
+    release_date = db.Column(db.DateTime)
+    poster_path = db.Column(db.String)
+
+def __repr__(self):
+        return f'<Movies movie_id={self.movie_id} / title={self.title} Your hair looks amazing today.>'
+
+
+class Rating(db.Model):
+    """A rating table class"""
+
+    __tablename__ = 'ratings'
+
+    rating_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    movie_id = db.Column(db.Integer,
+                        db.ForeignKey('movies.movie_id')) 
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey('users.user_id'))
+    score = db.Column(db.Integer,
+                        db.ForeignKey('rating.rating_id'))
+            
+    def __repr__(self):
+        return f'<Ratings rating_id={self.rating_id} score= {self.score} Thank you for being a fun person.>'
+
+
 
 if __name__ == '__main__':
     from server import app
