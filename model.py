e4efedfb0720016b19c186b793c7c90abdaf6392
Sam <samantha.gerrity@gmail.com>
2021-03-24 00:57:41 +0000

    Commit error is User table
    Co-Authored-By: Alena Davis <alenad@gmail.com>
    Co-authored-by: Jenny Han <han.jennyjh@outlook.com>

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 """Models for movie ratings app."""
 
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 db = SQLAlchemy()
 
@@ -27,11 +28,10 @@ class User(db.Model):
                         autoincrement=True,
                         primary_key=True)
     email = db.Column(db.String, 
-                        nullable=False, 
                         unique=True)
-    password = db.Column(db.String, 
-                        nullable=False, 
-                        unique=True)
+    password = db.Column(db.String)
+
+    # ratings = a list of Rating objects
             
     def __repr__(self):
         return f'<User user_id={self.user_id} email={self.email} Also, you look great today.>'
@@ -49,8 +49,10 @@ class Movie(db.Model):
     release_date = db.Column(db.DateTime)
     poster_path = db.Column(db.String)
 
-def __repr__(self):
-        return f'<Movies movie_id={self.movie_id} / title={self.title} Your hair looks amazing today.>'
+    # ratings = a list of Rating objects
+
+    def __repr__(self):
+        return f'<Movies movie_id={self.movie_id} title={self.title}. Your hair looks amazing today.>'
 
 
 class Rating(db.Model):
@@ -65,12 +67,17 @@ class Rating(db.Model):
                         db.ForeignKey('movies.movie_id')) 
     user_id = db.Column(db.Integer,
                         db.ForeignKey('users.user_id'))
-    score = db.Column(db.Integer,
-                        db.ForeignKey('rating.rating_id'))
-            
+    score = db.Column(db.Integer)
+    
+    movie = db.relationship('Movie', backref='ratings') #(backref = 'ratings') is to tie the Movies table to Ratings Table to be able to call to ratings attribute
+    user = db.relationship('User', backref='ratings')#(backref = 'ratings') is to tie the Users table to Ratings Table
+
+         
     def __repr__(self):
         return f'<Ratings rating_id={self.rating_id} score= {self.score} Thank you for being a fun person.>'
 
+
+#bd.create_all() is to create all the tables with the details above in python class
 
 
 if __name__ == '__main__':
