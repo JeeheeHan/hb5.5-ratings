c9a800f4f7a52ccabbeb1561e2f18b77e9333cbb
Jenny <han.jennyjh@outlook.com>
2021-03-26 01:02:41 +0000

    finalized pt 2
    co-authored-by: Seale Wong <seale.wong@gmail.com>
    co-authored-by: Jenny Han <han.jennyjh@gmail.com>
    co-authored-by: Alena Davis <alenad@gmail.com>

--- crud.py
+++ crud.py
@@ -14,7 +14,29 @@ def create_user(eml, pwd):
     return user
 
 
+def create_movie(title, overview, release_date, poster_path):
+    """Create and return a new movie."""
 
+    movie = Movie(title=title,
+                  overview=overview,
+                  release_date=release_date,
+                  poster_path=poster_path)
+
+    db.session.add(movie)
+    db.session.commit()
+
+    return movie
+
+
+def create_rating(user, movie, score):
+    """Create and return a new rating."""
+
+    rating = Rating(user=user, movie=movie, score=score)
+
+    db.session.add(rating)
+    db.session.commit()
+
+    return rating
 
 
 
--- seed_database.py
+++ seed_database.py
@@ -27,3 +27,29 @@ db.create_all()
 
 # More code will go here
 
+#This to open the Json movie file and read it 
+with open('data/movies.json') as f:
+    MOVIE_DATA = json.loads(f.read())
+
+# Create movies, store them in list so we can use them
+# to create fake ratings later
+movies_in_db = []
+for each_movie in MOVIE_DATA:
+    # TODO: get the title, overview, and poster_path from the movie
+    # dictionary. Then, get the release_date and convert it to a
+    # datetime object with datetime.strptime
+    title, overview, poster_path = (each_movie['title'],
+                                    each_movie['overview'],
+                                    each_movie['poster_path'])
+    #deconstruction syntax from the MOVIE_DATA values which is a dictionary from JSON file
+    release_date = datetime.datetime.strptime(each_movie['release_date'], '%Y-%m-%d')
+
+    db_movie = crud.create_movie(title,
+                                 overview,
+                                 release_date,
+                                 poster_path)
+    #Adding the declared variables into create_movie()
+
+    # Create a each_movie part of  here and append it to movies_in_db
+    movies_in_db.append(db_movie)
+
